fe4b147c51b7cd3dc6aca5d5df8d9f530a55d6fc
AgataQuadros <quadrosagata@gmail.com>
2024-12-03 19:22:23 -0300

    Criando tabelas

--- atividades/atividade_1/3_criando_tabelas.py
+++ atividades/atividade_1/3_criando_tabelas.py
@@ -1,22 +1,26 @@
 import sqlite3
 
-
+# Conexão ao banco de dados
 conn = sqlite3.connect("c:/python_agata/ProjetoBancodeDados/atividades/atividade_1/importando_sql.db")
 cursor = conn.cursor()
 
+# Tabela de clientes
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS clientes(
-        nome TEXT NOT NULL PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
         cpf INTEGER NOT NULL,
         email TEXT NOT NULL,
         telefone INTEGER NOT NULL,
-        modo_pagamento NOT NULL,
+        modo_pagamento TEXT NOT NULL
     );
 """)
 
+# Tabela de site
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS site(
-        companias PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        companias TEXT NOT NULL,
         destinos TEXT NOT NULL,
         precos INTEGER NOT NULL,
         cupons TEXT NOT NULL,
@@ -24,24 +28,34 @@ cursor.execute("""
     );
 """)
 
+# Tabela de passagem
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS passagem(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
+        nome_cliente TEXT NOT NULL,
         assento TEXT NOT NULL,
         voo INTEGER,
-        destinos TEXT NOT NULL,
+        destino TEXT NOT NULL,
         data TEXT NOT NULL,
         horario TEXT NOT NULL,
         saida INTEGER NOT NULL,
-        companias PRIMARY KEY AUTOINCREMENT
+        compania_id INTEGER NOT NULL,
+        FOREIGN KEY (compania_id) REFERENCES site(id),
+        FOREIGN KEY (nome_cliente) REFERENCES clientes(nome),
+        FOREIGN KEY (destino) REFERENCES destinos(locais)
     );
 """)
 
+# Tabela de destinos
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS destinos(
-        locais TEXT NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        locais TEXT NOT NULL,
         aeroporto INTEGER NOT NULL,
         pontos_turisticos TEXT NOT NULL
     );
 """)
+
+# Salvar as alterações e fechar a conexão
+conn.commit()
+conn.close()
