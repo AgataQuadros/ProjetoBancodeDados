f5dd5eb59da2e3d82e5c07ba99fc45db27dcde36
AgataQuadros <quadrosagata@gmail.com>
2024-12-02 21:02:52 -0300

    Inicio da Atualização

---
+++ atividades/atividade_1/7_atualizando.py
@@ -0,0 +1,20 @@
+import os 
+import sqlite3
+
+
+conn = sqlite3.connect("c:/python_agata/ProjetoBancodeDados/atividades/atividade_1/importando_sql.db")
+cursor = conn.cursor()
+
+os.system('cls')
+
+nome_cliente = input("digite o nome do cliente: ")
+nova_idade = int(input("digite a nova idade: "))
+
+# atualisa a idade com base no nome do funciomario
+cursor.execute("UPDATE clientes SET idade = ? WHERE nome = ?",
+                (nova_idade, nome_cliente))
+
+# salva as alterações no bd
+conn.commit()
+print("Dados atualizados com sucesso!")
+conn.close()
