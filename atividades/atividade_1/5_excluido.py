b5908c1a6e5a281386f86f5a0f28b6f52f80a2fe
AgataQuadros <quadrosagata@gmail.com>
2024-12-03 21:03:40 -0300

    Excluindo dados bem especificos das tabelas

--- atividades/atividade_1/5_excluido.py
+++ atividades/atividade_1/5_excluido.py
@@ -7,11 +7,47 @@ cursor = conn.cursor()
 
 os.system('cls')
 
-nome_cliente = input("Digite o nome do cliente para excluir: ")
+pergunta = input("qual tabela consultar? ")
 
-cursor.execute("DELETE FROM clientes WHERE nome = ?", (nome_cliente,))
-conn.commit()
+if pergunta == "cliente":
+    nome_cliente = input("Digite o nome do cliente para excluir: ")
 
-print("cliente deletado com sucesso!")
+    cursor.execute("DELETE FROM clientes WHERE nome = ?", (nome_cliente,))
+    conn.commit()
 
-conn.close()
+    print("cliente deletado com sucesso!")
+
+    conn.close()
+    
+    
+elif pergunta == "site":
+    nome_cupom = input("Digite o cupom para não usar mais: ")
+
+    cursor.execute("DELETE FROM site WHERE cupons = ?", (nome_cupom,))
+    conn.commit()
+
+    print("cupom deletado com sucesso!")
+
+    conn.close()
+    
+    
+elif pergunta == "destinos":
+    ponto_turistico = input("Digite o pony=to turistico para tirar do seu mapa: ")
+
+    cursor.execute("DELETE FROM destinos WHERE pontos_turisticos = ?", (ponto_turistico,))
+    conn.commit()
+
+    print("ponto turistico deletado com sucesso!")
+
+    conn.close()
+    
+    
+elif pergunta == "passagem":
+    assento = input("Digite o assento da passagem para cancelar: ")
+
+    cursor.execute("DELETE FROM passagem WHERE assento = ?", (assento,))
+    conn.commit()
+
+    print("passagem cancelada com sucesso!")
+
+    conn.close()
