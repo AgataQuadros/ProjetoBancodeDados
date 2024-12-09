66f459bf2fcc3ad0d92d34d0fd7ac37e4b2bbd4f
AgataQuadros <quadrosagata@gmail.com>
2024-12-09 19:57:41 -0300

    Trabalho De Banco de Dados com Python

--- atividades/atividade_1/8_menu.py
+++ atividades/atividade_1/8_menu.py
@@ -1,32 +1,32 @@
 import os
+import consultando
+import excluindo
+import adicionando
+import atualizando
+
 
 os.system("cls")
 
-def exibir_menu():
-    while True:
-        print("\n=== Sistema de Gerenciamento de Passagens Aéreas ===")
-        print("1. Criar conta")
-        print("2. Cadastrar Voo")
-        print("3. Consultar passagem")
-        print("5. Atualizar Informação")
-        print("6. Excluir Registro")
-        print("7. Sair")
-        opcao = input("Escolha uma opção: ")
 
-        if opcao == '1':
-            pass
-        elif opcao == '2':
-            pass
-        elif opcao == '3':
-            pass
-        elif opcao == '4':
-            pass
-        elif opcao == '5':
-            pass
-        elif opcao == '6':
-            pass
-        elif opcao == '7':
-            print("Saindo do sistema. Até mais!")
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
+while True:
+    print("\n=== Sistema de Gerenciamento de Passagens Aéreas ===")
+    print("1. Adicionar informações")
+    print("2. Consultar tabelas")
+    print("3. Atualizar Informação")
+    print("4. Excluir Registro")
+    print("5. Sair")
+    opcao = input("Escolha uma opção: ")
+
+    elif opcao == '1':
+        adicionando.Adiciona()
+    elif opcao == '2':
+        consultando.Consulta()
+    elif opcao == '3':
+        atualizando.Atualiza()
+    elif opcao == '4':
+        excluindo.Exclui()
+    elif opcao == '5':
+        print("Saindo do sistema. Até mais!")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
